d7db2543311d1fd9fe0649de88483e5478e5cfd9
Kévin Froissart <kevin.froissart69@gmail.com>
2022-06-01 14:58:44 +0200

    tp4

---
+++ src/tp4/tp4.java
@@ -0,0 +1,16 @@
+package tp4;
+
+public class tp4 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+/**
+ * Exo 87.
+ *
+ * 1. Une fois la somme partagée aux deux partis, il suffit de la soustraire avec la valeur envoyée pour trouver la valeur de l'autre parti.
+ * 2. Le 2ᵉ point n'est pas sur car Bob peut déduire la valeur de d'Alice comme expliqué juste avant.
+ */
